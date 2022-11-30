5acf03ac08015c80d369b8f5b43bd9d9e9d1e1ff
coopsie <aislingkcooper@hotmail.com>
2022-11-30 09:13:17 +0000

    change to string

--- Objects/activity1.js
+++ Objects/activity1.js
@@ -7,7 +7,7 @@ const person = {
             "Mr Brightside",
             "Valerie"],
     sayHi(){
-        return `Hello my name is this name${this.name}`
+        return `Hello my name is ${this.name}`
     }
 }
 console.log(person.sayHi());
